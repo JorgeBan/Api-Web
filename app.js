d0ba61b672a82462876e15a66c79fb050e050117
JorgeBanegas <banegas392@gmail.com>
2021-11-04 10:12:13 -0400

    errores de sesion arreglados

--- app.js
+++ app.js
@@ -19,10 +19,17 @@ app.set('view engine', 'ejs');
 const bcryptjs = require("bcryptjs");
 
 const session = require("express-session");
+const  MemoryStore = require('memorystore')(session)
+
+
 app.use(session({
+	cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000 // prune expired entries every 24h
+    }),
     secret: "secret",
-    resave: true,
-    saveUninitialized: true
+    resave: false,
+    //saveUninitialized: true
 }))
 
 
